d8de63a2a4011522a276cc89c5499181b7573a8a
marcin <38013623+voinar@users.noreply.github.com>
2023-04-18 09:56:16 +0200

    Update resize.py

--- resize.py
+++ resize.py
@@ -2,7 +2,7 @@ from PIL import Image
 import glob, os
 
 size = 1440, 1440
-folder = (r"C:\Users\zzx\Desktop\photos\*.*")
+folder = (r"C:\[enter_your_images_folder_path]\*.*")
 count = 0
 quality = 70
 
@@ -13,10 +13,3 @@ for infile in glob.glob(folder):
     im.save(str(count) + ".jpg", "JPEG", quality=quality)
     count = count + 1
     print(str(count))
-
-#import os
-#path = (r"C:/Users/zzx/Desktop/urodziny_jacka_33/")
-#files = os.listdir(path)
-
-#for index, file in enumerate(files):
-#    os.rename(os.path.join(path, file), os.path.join(path, '_urodziny_33 _jacka_2020_06'.join([str(index), '.jpg'])))
